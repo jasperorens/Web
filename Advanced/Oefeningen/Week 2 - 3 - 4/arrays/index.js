253d84e8e98fbabd54b77b358bc50479b360f591
Jasper Orens <12202416@student.pxl.be>
2023-03-03 12:07:06 +0100

    new files

---
+++ Advanced/Oefeningen/Week 2 - 3 - 4/arrays/index.js
@@ -0,0 +1,15 @@
+let producten = ['keyboard', 'kaas', 'mousepad'];
+let weekdagen = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
+
+document.getElementById('hoofding');
+document.getElementsByClassName('pxl') [0].innerHTML = 'PXL';
+
+for (let i = 0; i < weekdagen.length; i++) {
+    //do something
+}
+
+let producten = [
+    {naam: 'appel', prijs: 5},
+    {naam: 'appelsien', prijs: 2},
+    {naam: 'peer', prijs: 1}
+]
